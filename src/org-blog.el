fd34c3d3929e09d831b2d19a4c5e099ced6f81b6
Eric Nguyen <33969138+airicbear@users.noreply.github.com>
2020-01-03 09:29:47 -0500

    Use org-publish
    I started using org-publish.
    I barely have a clue what I'm doing, but it works.
    That's what matters, mostly.

---
+++ src/org-blog.el
@@ -0,0 +1,58 @@
+(defvar org-blog-css
+  "<link rel=\"stylesheet\" type=\"text/css\" href=\"assets/css/main.css\">")
+
+(defvar org-blog-icon
+  "<link rel=\"icon\" href=\"favicon.ico\">")
+
+(defvar org-blog-head
+  (concat org-blog-icon
+          org-blog-css))
+
+(defvar topnav
+  (concat "<nav>"
+          "<p>"
+          "Navigation: "
+          "<a href=\"index.html\">Home</a> "
+          "<a href=\"archive.html\">Blog</a> "
+          "<a href=\"about.html\">About</a> "
+          "<a href=\"contact.html\">Contact</a> "
+          "</nav>"))
+
+(setq org-publish-project-alist
+      `(("orgfiles"
+         :base-directory "~/Workspace/airicbear.github.io/src/"
+         :recursive t
+         :publishing-directory "~/Workspace/airicbear.github.io/"
+         :publishing-function org-html-publish-to-html
+
+         :lang "en"
+         :with-toc t
+         :title airicbear.github.io
+         :email "tuk94307@temple.edu"
+         :with-headline-numbers t
+         :with-date t
+         :with-title t
+         :time-stamp-file t
+         :section-numbers t
+         :table-of-contents t
+         :with-email t
+         :with-smart-quotes t
+
+         :html-doctype "html5"
+         :html-head-include-scripts nil
+         :html-head-include-default-style nil
+         :html-html5-fancy t
+         :html-validation-link nil
+         :html-head ,org-blog-head
+         :html-preamble ,topnav
+         :style ,org-blog-css
+
+         ;; Sitemap settings
+         :auto-sitemap t
+         :sitemap-filename "archive.org"
+         :sitemap-title "Blog"
+         :sitemap-style list
+         :sitemap-sort-files anti-chronologically
+         ;; :sitemap-format-entry org-blog-sitemap-format-entry
+         ;; :sitemap-function org-blog-sitemap-function
+         )))
